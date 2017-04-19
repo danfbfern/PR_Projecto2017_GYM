3034f8c91c9aef6a9af52ff8fdc8ead8ef339ee0
Daniel Fernando <danielfernando.1608@gmail.com>
2017-04-19 17:15:36 +0100

    update

--- gym/gym/settings.py
+++ gym/gym/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
+    'client',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
