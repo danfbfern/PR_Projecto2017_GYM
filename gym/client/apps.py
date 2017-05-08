29f44f286f1c111dbe8ac2c3d4450ee5178d6202
oliveirajoaonuno <oliveirajoaonuno@gmail.com>
2017-05-08 23:29:19 +0100

    client diferente, cenas do youtuber, css ect,( ou seja o que o juno fez nao vale nada)

--- gym/client/apps.py
+++ gym/client/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class ClientConfig(AppConfig):
-    name = 'client'
+class MusicConfig(AppConfig):
+    name = 'music'
--- gym/client/urls.py
+++ gym/client/urls.py
@@ -1,17 +1,19 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import url
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index2, name='index2'),
-    url(r'^objetivos/$', views.objetivos,name='objectivos'),
-    url(r'^treinos/$', views.treinos, name='treinos'),
-url(r'^evolucao/$', views.evolucao, name='evolucao'),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<song_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^songs/(?P<filter_by>[a-zA_Z]+)/$', views.songs, name='songs'),
+    url(r'^create_album/$', views.create_album, name='create_album'),
+    url(r'^(?P<album_id>[0-9]+)/create_song/$', views.create_song, name='create_song'),
+    url(r'^(?P<album_id>[0-9]+)/delete_song/(?P<song_id>[0-9]+)/$', views.delete_song, name='delete_song'),
+    url(r'^(?P<album_id>[0-9]+)/favorite_album/$', views.favorite_album, name='favorite_album'),
+    url(r'^(?P<album_id>[0-9]+)/delete_album/$', views.delete_album, name='delete_album'),
 ]
-urlpatterns += staticfiles_urlpatterns()
-
