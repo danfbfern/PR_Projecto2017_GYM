27bbcd1401337edcf8c5672b525762fa429236c6
oliveirajoaonuno <oliveirajoaonuno@gmail.com>
2017-05-11 02:59:48 +0100

    mais coisas. nem me lembro o que.

--- gym/client/views.py
+++ gym/client/views.py
@@ -293,22 +293,22 @@ def perfil(request):
                 return render(request, 'login.html', {'error_message': 'Invalid login'})
         return render(request, 'login.html')
     else:
+        render(request,'client_pessoal.html')
         if request.method == 'POST':
             user_form = UserForm(request.POST, instance=request.user)
             profile_form = ProfileForm(request.POST, instance=request.user.profile)
             if user_form.is_valid() and profile_form.is_valid():
                 user_form.save()
                 profile_form.save()
-                # messages.success(request, _('Your profile was successfully updated!'))
-                print('sucesso')
-                return redirect('profile')
+                #messages.success(request, _('Your profile was successfully updated!'))
+                return redirect('client_pessoal')
             else:
-                # messages.error(request, _('Please correct the error below.'))
-                print('erro')
+                 print('lol')
+                 #messages.error(request, _('Please correct the error below.'))
         else:
-            user_form = UserForm(instance=request.user)
-            profile_form = ProfileForm(instance=request.user.profile)
-        return render(request, 'profile.html', {
+               user_form = UserForm(instance=request.user)
+               profile_form = ProfileForm(instance=request.user.profile)
+        return render(request, 'client_pessoal.html', {
             'user_form': user_form,
             'profile_form': profile_form
         })
